5939ae52552a78371325617078ff6fbde6bbd6a0
Katya Weicht <katya.weicht@gmail.com>
2025-06-12 01:26:48 -0600

    Add delete functionality

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState} from "react";
+
 import ToyCard from "./ToyCard";
 
-function ToyContainer({toys}) {
+function ToyContainer({toys, setToys}) {
+  const [deletedToy, setDeletedToy] = useState(null)
+
+  function handleDeleteToys(deletedToy) {
+    const updatedToys = toys.filter(toy => toy.id !== deletedToy.id)
+    setToys(updatedToys)
+  }
+
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard key={toy.id} toy={toy} />
+        <ToyCard key={toy.id} toy={toy} setDeletedToy={setDeletedToy} handleDeleteToys={handleDeleteToys} />
       ))}
     </div>
   );
